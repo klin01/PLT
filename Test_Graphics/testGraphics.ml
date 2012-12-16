7507f85d46740a870d008f6cb34192f13f1a7e0f
Papoj Thamjaroenporn <episer@gmail.com>
2012-12-16 04:31:35 -0500

    Update testGraphics to have falling animation!

--- Test_Graphics/testGraphics.ml
+++ Test_Graphics/testGraphics.ml
@@ -12,11 +12,41 @@ let color_from_rgb r g b =
   The (0,0) is bottom left.
   For some reason we need to specify PARENTHESIS before the numbers themselves..
 *)
-let create_rectangle x r g b =
-  Graphics.draw_rect(10) (100) (x) (2*x);
-  Graphics.draw_circle(200) (200) (x/2);
+let create_rectangle size x y r g b =
+  Graphics.draw_rect(x) (y) (size) (2*size);
+  (*Graphics.draw_circle(x+100) (y+100) (size/2);*)
   Graphics.set_color( color_from_rgb r g b );
-  Graphics.fill_rect(10) (100) (x) (2*x)
+  Graphics.fill_rect(x) (y) (size) (2*size)
+  in
+
+
+(*
+  ANIMATION YAY! First version: using for loop
+  Advantage: Easy to use. Don't have to worry about mutable int
+  Disadvantage: Cannot increment any other value than 1.
+*)
+let move_steps1 total step size = 
+  for i = 0 to total do 
+      Graphics.clear_graph ();
+      create_rectangle size (10) (200 - i) 255 0 0 ;
+      create_rectangle (size*2/3) (100) (400 - i) 123 12 200;
+      Thread.join(Thread.create(Thread.delay)(1. /. 24.)) done;
+  in
+
+(*
+  ANIMATION YAY! Second version: using while loop
+  Advantage: Can increment by arbitrary value. Here I implemented "step".
+  Disadvantage: Have to mess with mutable data. Shown below.
+*)
+let move_steps2 total step size = 
+  let i = ref 0 in
+  while !i < total do 
+      Graphics.clear_graph ();
+      create_rectangle size (10) (200 - !i) 255 0 0 ;
+      create_rectangle (size*2/3) (100) (400 - !i) 123 12 200;
+      i := !i + step;
+      Thread.join(Thread.create(Thread.delay)(1. /. 24.)) (* NOTICE NO semicolon before done;*)
+      done;
   in
 
 (*
@@ -25,8 +55,8 @@ let create_rectangle x r g b =
 *)
 let x = 200 in
   Graphics.open_graph " 600x800";
-  create_rectangle x 255 0 0 ;
-  create_rectangle (x*2/3) 123 12 200 ;
+
+  move_steps2 100 3 x;
 
   Thread.join(Thread.create(Thread.delay)(float_of_int 4000 /. 1000.0));
   Graphics.clear_graph ();;
